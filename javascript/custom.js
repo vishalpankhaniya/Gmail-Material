04e98829dab9c343c02db421f97c5cb9d630f579
vishalpankhaniya <vishal.pankhaniya786@gmail.com>
2019-02-28 16:47:37 +0530

    firstcommit

---
+++ javascript/custom.js
@@ -0,0 +1,8 @@
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.field1'));
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.field2'));
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.pass1'));
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.pass2'));
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.firstname'));
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.lastname'));
+// textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.username'));
+
